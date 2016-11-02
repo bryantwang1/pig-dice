e26ff3ab6e81f8f7fa8032f49544b2de21d4c696
Bryant Wang and Hal Fairless <bryant@>
2016-11-02 09:03:49 -0700

    Add script to form submit that creates a new player and adds it to a currentPlayers array on every submission.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+var currentPlayers = [];
+
 function player(playerName) {
   this.name = playerName;
   this.score = 0;
 }
 
 $(function(){
+  var numberOfPlayers = 0;
+
+  $("form#player-creation").submit(function(event) {
+    event.preventDefault();
+    
+    var playerName = $("input#player-name").val();
+
+    currentPlayers[numberOfPlayers] = new player(playerName);
+    numberOfPlayers++;
+  });
+
    var result = Math.floor((Math.random() * 6) + 1);
 
    $("#current-score").text(result);
