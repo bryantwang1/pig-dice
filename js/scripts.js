564d4912db00aaa104cf96e77bba9d0256317ecc
Bryant Wang and Hal Fairless <bryant@>
2016-11-02 09:49:11 -0700

    Add variable turnPoints scoped inside document ready to track how many points have been accumulated during the current turn. Add script to hold score button to add turnPoints to the players score whose turn it is.

--- js/scripts.js
+++ js/scripts.js
@@ -28,26 +28,37 @@ $(function(){
     $("#which-player").text(currentPlayers[0].name);
   });
 
+  var turnPoints = 0;
+
   $("button#roll-dice").click(function() {
     // Generates a whole number from 1 to 6.
     var roll = Math.floor((Math.random() * 6) + 1);
+    turnPoints += roll;
     $("#last-roll").text(roll);
+    $("#turn-points").text(turnPoints);
   });
 
   $("button#hold-score").click(function() {
+    // For loop that checks whose turn it is and changes it to the next player's turn.
     for(idx = 0; idx < currentPlayers.length; idx++) {
       if(currentPlayers[idx].turn) {
+        currentPlayers[idx].score += turnPoints;
         currentPlayers[idx].turn = false;
 
         if(idx === currentPlayers.length-1) {
           currentPlayers[0].turn = true;
+          $("#which-player").text(currentPlayers[0].name);
           break;
         } else {
           currentPlayers[idx+1].turn = true;
+          $("#which-player").text(currentPlayers[idx+1].name);
           break;
         }
       }
     }
+    turnPoints = 0;
+    $("#last-roll").text(0);
+    $("#turn-points").text(turnPoints);
   });
 
 });
