6d1649daeafdbb1db738e555541fe54aca2d6401
Bryant Wang and Hal Fairless <hal@>
2016-11-02 09:10:18 -0700

    Add script that allows users to click the roll button and generate a random whole number from 1 to 6.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
+// After players are created they get stuffed in here.
 var currentPlayers = [];
-
+// A constructor for creating player objects.
 function player(playerName) {
   this.name = playerName;
   this.score = 0;
 }
 
+// User Interface below this line.
+
 $(function(){
   var numberOfPlayers = 0;
-
+  // Creates a new player on form submit, adds it to currentPlayers, and increments the index of currentPlayers by 1 after it does so.
   $("form#player-creation").submit(function(event) {
     event.preventDefault();
-    
+
     var playerName = $("input#player-name").val();
 
     currentPlayers[numberOfPlayers] = new player(playerName);
     numberOfPlayers++;
   });
 
-   var result = Math.floor((Math.random() * 6) + 1);
+  $("button#roll-dice").click(function() {
+    console.log("Click!");
 
-   $("#current-score").text(result);
+    // Generates a whole number from 1 to 6.
+     var roll = Math.floor((Math.random() * 6) + 1);
+      $("#last-roll").text(roll);
+  });
+
 });
